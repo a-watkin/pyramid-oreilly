784c15fb018f68e0d189f38987f5fa8cb34ea412
a <awatkin.work@gmail.com>
2019-11-06 18:03:22 +0000

    app setup and settings

---
+++ mysite/__init__.py
@@ -0,0 +1,17 @@
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+from pyramid.view import view_config
+
+
+@view_config(route_name='list', renderer='list.jinja2')
+def list(request):
+  return dict()
+
+
+def main(global_config, **settings):
+  config = Configurator(settings=settings)
+  config.include('pyramid_jinja2')
+  config.add_route('list', '/')
+  config.scan()
+  return config.make_wsgi_app()
