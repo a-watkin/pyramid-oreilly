52d418ff604e062be8f44a4ba796f286cef5e173
a <awatkin.work@gmail.com>
2019-11-06 13:41:54 +0000

    multuple routes and arguments

--- app.py
+++ app.py
@@ -3,16 +3,29 @@ from pyramid.config import Configurator
 from pyramid.response import Response
 from pyramid.view import view_config
 
-@view_config(route_name='hello_world', renderer='hello.jinja2')
-def hello(request):
+@view_config(route_name='home_page', renderer='home.jinja2')
+def home(request):
+  return dict(site_name='MyApp')
+
+
+@view_config(route_name='hello', renderer='hello_world.jinja2')
+def hello_world(request):
   return dict(name='world')
+
+
+@view_config(route_name='hello', renderer='hello_name.jinja2',
+              request_param='name')
+def hello_name(request):
+  name = request.params.get('name')
+  return dict(name=name)
 
 
 # start by just running it with python app.py
 if __name__ == "__main__":
   config = Configurator()
   config.include('pyramid_jinja2')
-  config.add_route('hello_world', '/')   
+  config.add_route('home_page', '/')
+  config.add_route('hello', '/hello')
   # config.add_view(hello, route_name='hello_world')
 
   # finds view_config
