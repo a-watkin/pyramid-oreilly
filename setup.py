15b7c02d09cf4602afd29f3d5d3c0b5e35b0871e
a-watkin <awatkin.work@gmail.com>
2020-01-11 11:55:26 +0000

    update formatting

--- setup.py
+++ setup.py
@@ -5,16 +5,17 @@
 from setuptools import setup
 
 requires = [
-  'praymid',
-  'pyramid_jinja2',
-  # this doesn't seem to install when put here
-  # it does work from requirements.txt
-  'waitress'
+    "praymid",
+    "pyramid_jinja2",
+    # this doesn't seem to install when put here
+    # it does work from requirements.txt
+    "waitress",
 ]
-setup(name='mysite',
-  install_required=requires,
-  entry_points="""\
+setup(
+    name="mysite",
+    install_required=requires,
+    entry_points="""\
     [paste.app_factory]
     main = mysite:main
-    """
+    """,
 )
